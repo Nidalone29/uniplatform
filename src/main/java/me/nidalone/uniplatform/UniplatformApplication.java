028d1bca97daf682ff3a614170df52cb06db8a4f
Nidal Guerouaja <22301275+nidalone29@users.noreply.github.com>
2025-04-29 22:47:44 +0200

    Initial Commit
    Signed-off-by: Nidal Guerouaja <22301275+Nidalone29@users.noreply.github.com>

---
+++ src/main/java/me/nidalone/uniplatform/UniplatformApplication.java
@@ -0,0 +1,13 @@
+package me.nidalone.uniplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/nidalone/uniplatform/UniplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nidalone.uniplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
