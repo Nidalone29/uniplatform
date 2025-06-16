29f45c8f57932ae7d544e88a25ad1ada35337d5b
Nidal Guerouaja <22301275+Nidalone29@users.noreply.github.com>
2025-06-17 01:21:55 +0200

    fix CORS

--- src/main/java/me/nidalone/uniplatform/UniplatformApplication.java
+++ src/main/java/me/nidalone/uniplatform/UniplatformApplication.java
@@ -18,7 +18,10 @@ public class UniplatformApplication {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+        registry
+            .addMapping("/api/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
       }
     };
   }
