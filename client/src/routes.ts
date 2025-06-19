0df50d0c9e0f5614faf394ae77cede89beeba0e7
Nidal Guerouaja <22301275+Nidalone29@users.noreply.github.com>
2025-06-19 18:16:46 +0200

    Remove deprecated comment

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -6,10 +6,6 @@ import { MainPage } from "./pages/MainPage";
 import { TableDataSkeleton } from "./components/common/TableDataSkeleton";
 import { loadDataUni, loadDataCourse, loadDataExam } from "./api/loadData";
 
-// TODO IMPORTANT right now when we load a component in course or exam and we make
-// the navbar, we load using loadDataCourse and loadDataExam. These functions make an API call
-// to the backed that fetches another list of courses and exams!!! in the near future this will be refactored
-// with an appropriate DTO on the backend to just give back the name and slug
 export const router = createBrowserRouter([
   {
     Component: MainPage,
